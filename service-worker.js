20b9dbdcd984a979741449529bcdd9a69dd34a76
Leson <cls-leson@163.com>
2018-01-15 14:22:10 +0800

    Add files via upload
    打包的项目

---
+++ service-worker.js
@@ -0,0 +1,79 @@
+importScripts('/static/js/workbox-sw.prod.v2.1.2.js');/**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author lesonchan(cls-leson@163.com)
+ */
+
+const workboxSW = new WorkboxSW({
+    cacheId: 'lavas-cache',
+    ignoreUrlParametersMatching: [/^utm_/],
+    skipWaiting: true,
+    clientsClaim: true
+});
+
+// Define precache injection point.
+workboxSW.precache([
+  {
+    "url": "/index.html",
+    "revision": "ad73a02a583d1efc6d325a5424dfaa82"
+  },
+  {
+    "url": "/static/css/index.4b43edcd.css"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.ttf",
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.woff",
+    "revision": "012cf6a10129e2275d79d6adac7f3b02"
+  },
+  {
+    "url": "/static/js/index.4e2ccc26.js"
+  },
+  {
+    "url": "/static/js/manifest.d12877e0.js"
+  },
+  {
+    "url": "/static/js/vendor.21d0fec0.js"
+  },
+  {
+    "url": "/static/js/vue.bcacc9b1.js"
+  },
+  {
+    "url": "/static/js/workbox-sw.prod.v2.1.2.js",
+    "revision": "685d1ceb6b9a9f94aacf71d6aeef8b51"
+  }
+]);
+
+/**
+ * example runningCache with api
+ */
+// workboxSW.router.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workboxSW.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workboxSW.router.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workboxSW.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         cacheExpiration: {
+//             maxEntries: 100,
+//             maxAgeSeconds: 7 * 24 * 60 * 60
+//         },
+//         cacheableResponse: {
+//             statuses: [0, 200]
+//         }
+//     })
+// );
+workboxSW.router.registerNavigationRoute('/index.html');
